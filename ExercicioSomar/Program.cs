4368d1618683505a6f1160924c9f4e5e7904f268
Victor AEF <67162119+Victoraef@users.noreply.github.com>
2022-05-17 12:49:45 -0300

    Atividade de Somar em C#
    Executado para aula de Programação em C#
    uma Soma de  3 números exemplo :( 1 +3 + 5 =9 )

---
+++ ExercicioSomar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        somaAN(5, 3);
+        somaAN(1, 7);      
+        
+    }
+    public static void somaAN(int a, int n)
+    {
+        int cont = a;
+        string resultado = a + "+";
+        int aux = a;
+        for (int i = 1; i < n; i++)
+        {
+            cont += (a + i);
+            aux = a + i;
+            resultado += aux;
+            if (i != n - 1)
+            {
+                resultado += "+";
+            }
+        }
+
+        Console.WriteLine(resultado + "=" + cont);
+
+    }
+}
+
